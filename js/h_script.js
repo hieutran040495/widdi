60775f2e20c1bad23e9bb469793f8d440013235a
hieutran040495 <hieutran040495@gmail.com>
2019-03-10 21:50:14 +0700

    Resize image

--- js/h_script.js
+++ js/h_script.js
@@ -13,5 +13,37 @@ $(".owl-carousel").owlCarousel({
   autoPlay:true,
   autoPlayTimeout:500,
   autoPlayHoverPause:true
-  
 });
+
+$(window).scroll(function() {
+  var topPos = $(this).scrollTop();
+  if (topPos >= 527) {
+    $(".h-logo-text").addClass("h-logo-text--scroll");
+  } else {
+    $(".h-logo-text").removeClass("h-logo-text--scroll");
+  }
+})
+
+// Menu scroll animation
+// var invitation = $("#invitation").position();
+// var maxim = $("#maxim").position();
+// var story = $("#story").position();
+// var moment = $("#moment").position();
+// var event = $("#event").position();
+// var rsvp = $("#rsvp").position();
+
+// animationScroll(".link1", invitation);
+// animationScroll(".link2", maxim);
+// animationScroll(".link3", story);
+// animationScroll(".link4", moment);
+// animationScroll(".link5", event);
+// animationScroll(".link6", rsvp);
+
+// function animationScroll(id, el) {
+//   $(id).click(function() {
+//     $('html, body').animate({
+//       scrollTop: el.top
+//     }, 500);
+//     return false;
+//   });
+// }
