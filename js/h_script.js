19262ff98aac9cf5922386a090d0d8c9dcd75969
hieutran040495 <hieutran040495@gmail.com>
2019-03-14 16:49:56 +0700

    Add animation scroll

--- js/h_script.js
+++ js/h_script.js
@@ -1,49 +1,49 @@
-$(".h-image-bck").each(function() {
-  var image = $(this).attr("data-h-image");
-  if (image) {
-    $(this).css("background-image", "url(" + image + ")");
-  }
+var target;
+
+$(document).ready(function() {
+  $(".h-image-bck").each(function() {
+    var image = $(this).attr("data-h-image");
+    if (image) {
+      $(this).css("background-image", "url(" + image + ")");
+    }
+  })
+  
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    items:1,
+    responsive: false,
+    autoPlay:true,
+    autoPlayTimeout:500,
+    autoPlayHoverPause:true
+  });
+
+  $('.h-link').bind('click', function(e) {
+    e.preventDefault();
+
+    // Add class active
+    $('.gla_parent .h-link.active').removeClass('active');
+    $(e.target).addClass('active');
+
+    // Animation scroll
+    target = $(this).attr("href");
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+      }, 600, function() {
+      location.hash = target;
+    });
+    return false;
+  }) 
+  
 })
 
-$(".owl-carousel").owlCarousel({
-  loop:true,
-  margin:10,
-  items:1,
-  responsive: false,
-  autoPlay:true,
-  autoPlayTimeout:500,
-  autoPlayHoverPause:true
-});
-
-$(window).scroll(function() {
-  var topPos = $(this).scrollTop();
-  if (topPos >= 527) {
-    $(".h-logo-text").addClass("h-logo-text--scroll");
-  } else {
-    $(".h-logo-text").removeClass("h-logo-text--scroll");
-  }
-})
-
-// Menu scroll animation
-// var invitation = $("#invitation").position();
-// var maxim = $("#maxim").position();
-// var story = $("#story").position();
-// var moment = $("#moment").position();
-// var event = $("#event").position();
-// var rsvp = $("#rsvp").position();
-
-// animationScroll(".link1", invitation);
-// animationScroll(".link2", maxim);
-// animationScroll(".link3", story);
-// animationScroll(".link4", moment);
-// animationScroll(".link5", event);
-// animationScroll(".link6", rsvp);
-
-// function animationScroll(id, el) {
-//   $(id).click(function() {
-//     $('html, body').animate({
-//       scrollTop: el.top
-//     }, 500);
-//     return false;
+// $(window).scroll(function() {
+//   var scrollDistance = $(window).scrollTop();
+  
+//   $('.page-section').each(function(i) {
+//     if ($(this).position().top <= scrollDistance) {
+//       $('.gla_parent .h-link.active').removeClass('active');
+//       $('.gla_parent .h-link').eq(i).addClass('active');
+//     }
 //   });
-// }
+// }).scroll();
